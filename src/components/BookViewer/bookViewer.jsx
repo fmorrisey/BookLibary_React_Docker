f9099be9f94ca90ef85ab3de883ea2717fdb9931
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-01-21 16:51:58 -0600

    created and moved bookviewer code

---
+++ src/components/BookViewer/bookViewer.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function bookViewer(props) {
+  return (
+    <div className="row row-spacer">
+      <div className="col-md-4">
+        <button onClick={() => this.goToPreviousBook()}>Previous Book</button>
+      </div>
+      <div className="col-md-4">
+        <h1>{this.books[this.state.bookNumber].title}</h1>
+        <h4>{this.books[this.state.bookNumber].author}</h4>
+      </div>
+      <div className="col-md-4">
+        <button onClick={() => this.goToNextBook()}>Next Book</button>
+      </div>
+    </div>
+  );
+}
