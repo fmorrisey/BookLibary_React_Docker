0a07beffccb94616211f9264637f213cd6e5e0bf
Forrest Morrisey <forrestmorrisey@gmail.com>
2021-01-21 16:45:46 -0600

    book browsing added

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 ### DOCKERIZED REACT APP
+Created a React app inside of a Docker Container
--- src/components/app.jsx
+++ src/components/app.jsx
@@ -1,17 +1,60 @@
-import React, { Component } from 'react';
-import TitleBar from './TitleBar/titleBar';
+import React, { Component } from "react";
+import TitleBar from "./TitleBar/titleBar";
 
 //Class or Stateful Component
 export default class App extends Component {
-    state = {
+  constructor(props) {
+    super(props);
+    this.books = [
+      { title: "Ready Player One", author: "Ernest Cline" },
+      { title: "All the Light We Cannot See", author: "Anthony Doerr" },
+      { title: "The First and Last Freedom", author: "Jiddu Krishnamurti" },
+    ];
+    this.state = {
+      bookNumber: 2,
+    };
+  }
 
+  goToNextBook() {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber++;
+    if (tempBookNumber === this.books.length) {
+      tempBookNumber = 0;
+    }
+    this.setState({
+      bookNumber: tempBookNumber,
+    });
+  }
+
+  goToPreviousBook() {
+    let tempBookNumber = this.state.bookNumber;
+    tempBookNumber--;
+    if (tempBookNumber < 0) {
+        tempBookNumber = this.books.length - 1;
     }
 
-    render() {
-        return (
-            <div className="container-fluid">
-                <TitleBar />
-            </div>
-        );
-    }
+      this.setState({
+        bookNumber: tempBookNumber,
+      });
+  }
+
+  render() {
+    return (
+      <div className="container-fluid">
+        <TitleBar />
+        <div className="row row-spacer">
+          <div className="col-md-4">
+            <button onClick={() => this.goToPreviousBook()}>Previous Book</button>
+          </div>
+          <div className="col-md-4">
+            <h1>{this.books[this.state.bookNumber].title}</h1>
+            <h4>{this.books[this.state.bookNumber].author}</h4>
+          </div>
+          <div className="col-md-4">
+            <button onClick={() => this.goToNextBook()}>Next Book</button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
